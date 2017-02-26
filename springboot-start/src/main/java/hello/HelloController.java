f421618f950dedb9d67a67d88131258c0465c6fb
binblog <bininhere@163.com>
2017-02-26 16:16:12 +0800

    springboot-start

---
+++ springboot-start/src/main/java/hello/HelloController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by bin on 2017/2/26.
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+}
