eacbd76974c3695185a08a6e497f22b9cd3e2466
binblog <bininhere@163.com>
2017-02-26 16:40:28 +0800

    rest start

---
+++ springboot-start/src/test/java/rest/ApplicationTests.java
@@ -0,0 +1,85 @@
+package rest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import rest.controller.UserController;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+/**
+ * Created by bin on 2017/2/26.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = MockServletContext.class)
+@WebAppConfiguration
+public class ApplicationTests {
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
+    }
+
+    @Test
+    public void testUserController() throws Exception {
+        // 测试UserController 
+        RequestBuilder request = null;
+
+        // 1、get查一下user列表，应该为空 
+        request = get("/users/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[]")));
+
+        // 2、post提交一个user 
+        request = post("/users/")
+                .param("id", "1")
+                .param("name", "测试大师")
+                .param("age", "20");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("success")));
+
+        // 3、get获取user列表，应该有刚才插入的数据 
+        request = get("/users/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"测试大师\",\"age\":20}]")));
+
+        // 4、put修改id为1的user 
+        request = put("/users/1")
+                .param("name", "测试终极大师")
+                .param("age", "30");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("success")));
+
+        // 5、get一个id为1的user 
+        request = get("/users/1");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("{\"id\":1,\"name\":\"测试终极大师\",\"age\":30}")));
+
+        // 6、del删除id为1的user 
+        request = delete("/users/1");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("success")));
+
+        // 7、get查一下user列表，应该为空 
+        request = get("/users/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[]")));
+
+    }
+}
