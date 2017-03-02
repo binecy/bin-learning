dfa075b1c6b2b3fc5250552e9b828e22ac289c99
linagguobin <bin.liang@jfz.com>
2017-03-02 10:58:52 +0800

    add netty http

---
+++ netty-start/src/main/java/com/netty/start/http/HttpSnoopServerInitializer.java
@@ -0,0 +1,34 @@
+package com.netty.start.http;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.ssl.SslContext;
+
+/**
+ * Created by bin on 2017/3/2.
+ */
+public class HttpSnoopServerInitializer  extends ChannelInitializer<SocketChannel> {
+    private final SslContext sslCtx;
+
+    public HttpSnoopServerInitializer(SslContext sslCtx) {
+        this.sslCtx = sslCtx;
+    }
+
+    @Override
+    public void initChannel(SocketChannel ch) {
+        ChannelPipeline p = ch.pipeline();
+        if (sslCtx != null) {
+            p.addLast(sslCtx.newHandler(ch.alloc()));
+        }
+        p.addLast(new HttpRequestDecoder());
+        // Uncomment the following line if you don't want to handle HttpChunks.
+        //p.addLast(new HttpObjectAggregator(1048576));
+        p.addLast(new HttpResponseEncoder());
+        // Remove the following line if you don't want automatic content compression.
+        //p.addLast(new HttpContentCompressor());
+        p.addLast(new HttpSnoopServerHandler());
+    }
+}
