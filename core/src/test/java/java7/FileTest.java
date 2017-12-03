3244c8c41d79f9d2c07182af7cbd50bbef8141c9
binecy <bininhere@163.com>
2017-12-03 16:12:12 +0800

    fileTest

--- core/src/test/java/java7/FileTest.java
+++ core/src/test/java/java7/FileTest.java
@@ -2,13 +2,15 @@ package java7;
 
 import org.junit.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.net.URL;
+import java.nio.file.*;
 
 /**
  * Created by bin on 2017/11/12.
@@ -17,26 +19,36 @@ public class FileTest {
 
     @Test
     public void testPath() throws URISyntaxException {
-
+        // 获取local.properties文件
         URI localProUri = this.getClass().getClassLoader().getResource("local.properties").toURI();
-        System.out.println(localProUri);
-
+//        System.out.println(localProUri);
+        // 转换为Path对象, 指向 .../resources/test/local.properties
         Path localProPath = Paths.get(localProUri);
+        System.out.println(localProPath.toAbsolutePath());
 
+        // 解析localProPath兄弟目录, 指向 .../resources/test/xml
         Path xmlPath = localProPath.resolveSibling("xml");
         System.out.println(xmlPath.toAbsolutePath());
-
+        // 解析xmlPath目录目录, 指向 .../resources/test/xml/local.xml
         Path localXmlPath = xmlPath.resolve("local.xml");
+
+        // 转换为绝对路径
         System.out.println(localXmlPath.toAbsolutePath());
 
+        // 获取文件名
         System.out.println(localXmlPath.getFileName());
+
+        System.out.println(localXmlPath.getRoot());
+
+        System.out.println(localXmlPath.getParent());
+
 
     }
 
     @Test
     public void testFileRead() throws URISyntaxException, IOException {
-        URI localProUri = this.getClass().getClassLoader().getResource("local.properties").toURI();
-        Path localProPath = Paths.get(localProUri);
+        Path localProPath = Paths.get(this.getClass().getClassLoader().getResource("local.properties").toURI());
+        // 读取文件中所以的字节
         byte[] localProBytes = Files.readAllBytes(localProPath);
         System.out.println(new String(localProBytes));
     }
@@ -44,19 +56,18 @@ public class FileTest {
 
     @Test
     public void testFileCopy() throws URISyntaxException, IOException {
-        URI localProUri = this.getClass().getClassLoader().getResource("local.properties").toURI();
-        Path localProPath = Paths.get(localProUri);
-
-        Path targetPath = Files.copy(localProPath, localProPath.getParent().resolve("dev.propeties"));
+        Path localProPath = Paths.get(this.getClass().getClassLoader().getResource("local.properties").toURI());
+        // 复制文件, StandardCopyOption.REPLACE_EXISTING表示替换已存在的文件
+        Path targetPath = Files.copy(localProPath, localProPath.getParent().resolve("dev.properties"), StandardCopyOption.REPLACE_EXISTING);
         System.out.println(targetPath.toAbsolutePath());
 
     }
 
     @Test
     public void testAppend() throws URISyntaxException, IOException {
-        URI localProUri = this.getClass().getClassLoader().getResource("local.properties").toURI();
-        Path localProPath = Paths.get(localProUri);
+        Path localProPath = Paths.get(this.getClass().getClassLoader().getResource("local.properties").toURI());
 
+        // 追加文件,StandardOpenOption.APPEND表示追加
         Files.write(localProPath, "group=local".getBytes(), StandardOpenOption.APPEND);
 
 //        Files.move()
@@ -64,6 +75,11 @@ public class FileTest {
 //        Files.createDirectories()
 //        Files.createFile()
 //        Files.getAttribute()
+
+
+
     }
+
+
 
 }
